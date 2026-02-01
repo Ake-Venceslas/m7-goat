5a61205bfc3a7ca338b8096d9cec353a85d8d73a
Ake-Venceslas <akevenceslas850@gmail.com>
2026-02-01 23:43:01 +0100

    deploy 2

--- src/components/Footer/FooterComponents.tsx
+++ src/components/Footer/FooterComponents.tsx
@@ -3,12 +3,12 @@
 import Link from "next/link";
 import { Instagram, Facebook, Youtube } from "lucide-react";
 import { useLanguage } from "@/src/context/LanguageContext";
-import { motion } from "framer-motion";
+import { motion, type Variants } from "framer-motion";
 
 const FooterComponents = () => {
   const { t } = useLanguage();
 
-  const containerVariants = {
+  const containerVariants: Variants = {
     hidden: { opacity: 0 },
     visible: {
       opacity: 1,
@@ -19,14 +19,14 @@ const FooterComponents = () => {
     },
   };
 
-  const itemVariants = {
+  const itemVariants: Variants = {
     hidden: { opacity: 0, y: 20 },
     visible: {
       opacity: 1,
       y: 0,
       transition: {
         duration: 0.5,
-        ease: "easeOut",
+        ease: "easeOut" as const,
       },
     },
   };
